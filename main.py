80cd5a1995911886d3e1a08fb301500037e60787
区荣杰 <1309908437@qq.com>
2021-02-20 21:24:33 +0800

    fix some bugs

--- main.py
+++ main.py
@@ -15,33 +15,37 @@ def release_signal(timer):
 
 def get_counter(Tim):
     count = Tim.counter()
-    Tim.counter(0)
-    return count
+    Tim.counter(500)
+    return count - 500
 
 
-def set_pwm(M1_A_Percent, M1_B_Percent, M2_A_Percent, M2_B_Percent):
-    M1_A.pulse_width_percent(M1_A_Percent)
-    M1_B.pulse_width_percent(M1_B_Percent)
-    M2_A.pulse_width_percent(M2_A_Percent)
-    M2_B.pulse_width_percent(M2_B_Percent)
+def set_pwm(M1_A_Value, M1_B_Value, M2_A_Value, M2_B_Value):
+    M1_A.pulse_width(M1_A_Value)
+    M1_B.pulse_width(M1_B_Value)
+    M2_A.pulse_width(M2_A_Value)
+    M2_B.pulse_width(M2_B_Value)
 
 
 def sw_callback():
     global speed
     if ((speed+10) == 50):
-        speed = 0
+        speed = -40
     else:
         speed = speed + 10
 
 
 EncoderM1 = pyb.Timer(8)
 EncoderM1.init(prescaler=1, period=1000, mode=pyb.Timer.UP)
-print(stm.mem16[stm.TIM8 + stm.TIM_CR1])
+#print(stm.mem16[stm.TIM8 + stm.TIM_CR1])
 EncoderM1.channel(1, mode=pyb.Timer.ENC_AB, pin=pyb.Pin.board.Y1)
 EncoderM1.channel(2, mode=pyb.Timer.ENC_AB, pin=pyb.Pin.board.Y2)
-print(stm.mem16[stm.TIM8 + stm.TIM_SMCR])
+EncoderM1.counter(500)
+#print(stm.mem16[stm.TIM8 + stm.TIM_SMCR])
 
-M_timer = pyb.Timer(2, freq=1000)
+#M_timer = pyb.Timer(2, freq=1000)
+M_timer = pyb.Timer(2)
+M_timer.init(prescaler=11, period=6999)
+print(stm.mem32[stm.TIM2 + stm.TIM_ARR])
 M1_A = M_timer.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.X1)
 M1_B = M_timer.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.X2)
 M2_A = M_timer.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.X3)
@@ -58,14 +62,15 @@ Velocity_Kp = 50
 Velocity_Ki = 10
 Velocity_Kd = 0
 '''
-Velocity_Kp = -1.2
-Velocity_Ki = -0.02
-Velocity_Kd = -1
-bias_Integral = 0.0
-bias = 0.0
-last_bias = 0.0
-pwm_percent = 0.0
-speed = 0
+Velocity_Kp = -5
+Velocity_Ki = 0
+Velocity_Kd = 0
+bias_Integral = 0
+bias = 0
+last_bias = 0
+pwm_ValueA = 0
+pwm_ValueB = 0
+speed = 10
 
 while (1):
     if (polling_flag):
@@ -79,16 +84,20 @@ while (1):
             bias_Integral = 15000
         if (bias_Integral < -15000):
             bias_Integral = -15000
-        pwm_percent = pwm_percent + (Velocity_Kp*error + Velocity_Ki *
+        pwm_ValueA = pwm_ValueA + (Velocity_Kp*error + Velocity_Ki *
                     bias_Integral + (error-last_bias)*Velocity_Kd)
-        print("pwm_percent=", pwm_percent)
-        if (pwm_percent > 100):
-            pwm_percent = 100
-        if (pwm_percent < -100):
-            pwm_percent = -100
+        pwm_ValueB = pwm_ValueB - (Velocity_Kp*error + Velocity_Ki *
+                    bias_Integral + (error-last_bias)*Velocity_Kd)
+        if (pwm_ValueA < 0):    pwm_ValueA = 0
+        if (pwm_ValueA > 6999): pwm_ValueA = 6999
+        if (pwm_ValueB < 0):    pwm_ValueB = 0
+        if (pwm_ValueB > 6999): pwm_ValueB = 6999
         last_bias = error
-        set_pwm(math.fabs(pwm_percent), 0, 0, 0)
+        set_pwm(int(pwm_ValueA), int(pwm_ValueB), 0, 0)
+        #print(stm.mem32[stm.TIM2 + stm.TIM_CCR4])
         print("bias_Integral=", bias_Integral)
+        print("valueA_pre=", stm.mem32[stm.TIM2 + stm.TIM_CCR1]/70, "%")
+        print("valueB_pre=", stm.mem32[stm.TIM2 + stm.TIM_CCR2]/70, "%")
         
 
 '''
